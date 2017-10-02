fdffa7416d6ba20528019fa5c0e07b4400c9de8b
gelder <gelder@gmx.de>
2017-10-02 19:33:32 +0200

    Reformat security configuration for better readability

--- src/main/java/pl/aetas/springbootadmin/SecurityConfiguration.java
+++ src/main/java/pl/aetas/springbootadmin/SecurityConfiguration.java
@@ -5,9 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * @see "https://github.com/codecentric/spring-boot-admin/blob/master/spring-boot-admin-samples/spring-boot-admin-sample/src/main/java/de/codecentric/boot/admin/SpringBootAdminApplication.java"
- *
  * @author gelder
+ * @see "https://github.com/codecentric/spring-boot-admin/blob/master/spring-boot-admin-samples/spring-boot-admin-sample/src/main/java/de/codecentric/boot/admin/SpringBootAdminApplication.java"
  */
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
@@ -15,17 +14,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
-		// Page with login form is served as /login.html and does a POST on /login
-		http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+		// Pa	ge with login form is served as /login.html and does a POST on /login
+		http.formLogin()
+			.loginPage("/login.html")
+			.loginProcessingUrl("/login")
+			.permitAll();
 		// The UI does a POST on /logout on logout
-		http.logout().logoutUrl("/logout");
+		http.logout()
+			.logoutUrl("/logout");
 		// The ui currently doesn't support csrf
-		http.csrf().disable();
+		http.csrf()
+			.disable();
 
 		// Requests for the login page and the static assets are allowed
-		http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
+		http.authorizeRequests()
+			.antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+			.permitAll();
 		// ... and any other request needs to be authorized
-		http.authorizeRequests().antMatchers("/**").authenticated();
+		http.authorizeRequests()
+			.antMatchers("/**")
+			.authenticated();
 
 		// Enable so that the clients can authenticate via HTTP basic for registering
 		http.httpBasic();
